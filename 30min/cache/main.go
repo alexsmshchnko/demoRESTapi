866373d774abb724fbeb48811d41e21c4d32fb4b
alexsmshchnko <quilematiss@yandex.ru>
2025-02-25 15:37:10 +0300

    fix: make cache isolated

--- 30min/cache/main.go
+++ 30min/cache/main.go
@@ -46,7 +46,7 @@ type Order struct {
 const TTL = 2 * time.Second
 
 type CacheItem struct {
-	*Profile
+	Profile
 	expireAt time.Time
 }
 
@@ -56,14 +56,18 @@ type Cache struct {
 	ttl   time.Duration
 }
 
-func NewCache() *Cache {
-	return &Cache{
+func NewCache() (c *Cache) {
+	c = &Cache{
 		items: map[string]*CacheItem{},
 		ttl:   TTL,
 	}
+
+	go c.Cleanup()
+
+	return
 }
 
-func (c *Cache) Get(uuid string) (p *Profile, found bool) {
+func (c *Cache) Get(uuid string) (p Profile, found bool) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
@@ -81,12 +85,23 @@ func (c *Cache) Get(uuid string) (p *Profile, found bool) {
 	return
 }
 
-func (c *Cache) Set(p *Profile) {
+func (c *Cache) Set(p Profile) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
+	orders := make([]*Order, len(p.Orders))
+	for i, v := range p.Orders {
+		v := *v
+		orders[i] = &v
+	}
+
+	var profile Profile
+	profile.UUID = p.UUID
+	profile.Name = p.Name
+	profile.Orders = orders
+
 	c.items[p.UUID] = &CacheItem{
-		Profile:  p,
+		Profile:  profile,
 		expireAt: time.Now().Add(c.ttl),
 	}
 }
@@ -107,29 +122,32 @@ func (c *Cache) Cleanup() {
 
 func main() {
 	cache := NewCache()
-	go cache.Cleanup()
 
 	const uuidToFind = "aslkfj-123"
 
 	r, f := cache.Get(uuidToFind)
-	println(r, f) //false
+	println(r.Name, r.Orders, f) //false
 
-	cache.Set(
-		&Profile{
-			UUID:   uuidToFind,
-			Name:   "user1",
-			Orders: []*Order{{UUID: "312", Value: "test1"}},
-		},
-	)
+	p := Profile{
+		UUID:   uuidToFind,
+		Name:   "user1",
+		Orders: []*Order{{UUID: "312", Value: "test1"}},
+	}
+
+	cache.Set(p)
+
+	//check cache is isolated and safe
+	p.Name = "user0"
+	p.Orders[0].UUID = "000"
 
 	time.Sleep(1 * time.Second)
 
 	r, f = cache.Get(uuidToFind)
-	println(r.Name, r.Orders, f) //true
+	println(r.Name, r.Orders[0].UUID, f) //true
 
 	time.Sleep(3 * time.Second)
 
 	r, f = cache.Get(uuidToFind)
-	println(r, f) //false
+	println(r.Name, r.Orders, f) //false
 
 }
