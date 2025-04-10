13d7ccda2d9d3fc9a4ce139fdf5a5f033561275e
alexsmshchnko <quilematiss@yandex.ru>
2025-04-10 15:19:59 +0300

    feat: add multiple URLs to parse

--- 30min/scrapler/main.go
+++ 30min/scrapler/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"net/http"
+	"path"
+	"sync"
 	"time"
 
 	"golang.org/x/net/html"
 )
-
-const ULR_TO_PARSE = "https://go.dev"
 
 func main() {
 	if err := run(); err != nil {
@@ -15,32 +15,22 @@ func main() {
 	}
 }
 
+var ULRS_TO_PARSE = []string{"https://go.dev", "https://en.wikipedia.org", "https://github.com/"}
+
 func run() error {
 	client := http.Client{Timeout: 2 * time.Second}
-
-	resp, err := client.Get(ULR_TO_PARSE)
-	if err != nil {
-		return err
-	}
-
-	doc, err := html.Parse(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		return err
-	}
 
 	var extractURLs func(n *html.Node, pURL string) (s []string)
 	extractURLs = func(n *html.Node, pURL string) (s []string) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, attr := range n.Attr {
-				if attr.Key == "href" {
+				if attr.Key == "href" && len(attr.Val) > 0 {
 					if []rune(attr.Val)[0] == '/' {
 						s = append(s, pURL+attr.Val)
 					} else {
 						pURL = attr.Val
 						s = append(s, attr.Val)
 					}
-					// println(attr.Val)
 				}
 			}
 		}
@@ -52,9 +42,39 @@ func run() error {
 		return s
 	}
 
-	for _, v := range extractURLs(doc, ULR_TO_PARSE) {
-		println(v)
+	var scrap func(c *http.Client, url string, wg *sync.WaitGroup) (err error)
+	scrap = func(c *http.Client, url string, wg *sync.WaitGroup) (err error) {
+		defer wg.Done()
+
+		resp, err := c.Get(url)
+		if err != nil {
+			return err
+		}
+
+		doc, err := html.Parse(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return err
+		}
+
+		uniquePath := make(map[string]interface{})
+		for _, s := range extractURLs(doc, url) {
+			uniquePath[s] = struct{}{}
+		}
+
+		for v := range uniquePath {
+			println(path.Base(url), "->", v)
+		}
+		return
 	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(ULRS_TO_PARSE))
+	for _, u := range ULRS_TO_PARSE {
+		go scrap(&client, u, wg)
+	}
+
+	wg.Wait()
 
 	return nil
 }
